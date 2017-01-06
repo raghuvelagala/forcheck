9afc7de552206cf4a9cc4fa622757eb14539e0a9
Raghu Velagala <raghu@velagala.org>
2017-01-06 17:16:07 -0500

    First Commit

---
+++ src/check.java
@@ -0,0 +1,18 @@
+
+public class check {
+		
+		public static void main(String[] args){
+			
+			int arrNum[] = new int[10];
+			
+			for(int i=0;i<10;i++){
+				arrNum[i] = i;
+			}
+			arrNum[2] = 3000;
+			for(int x : arrNum){
+				System.out.println(x);				
+			}
+			
+		}
+
+}
